4e64e69dd70ee394ba494772d2e7c68029a0da3b
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-26 16:37:00 +0000

    Wrote test for running SignSic as UIJob. runInUI() never reached, filed as bug 1117.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10757 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ds.tests/src/net/bioclipse/ds/tests/TestSignSIc.java
+++ plugins/net.bioclipse.ds.tests/src/net/bioclipse/ds/tests/TestSignSIc.java
@@ -23,6 +23,7 @@ import net.bioclipse.ds.Activator;
 import net.bioclipse.ds.business.IDSManager;
 import net.bioclipse.ds.model.ITestResult;
 import net.bioclipse.ds.model.impl.DSException;
+import net.bioclipse.jobs.BioclipseUIJob;
 
 import org.junit.Test;
 import org.openscience.cdk.interfaces.IAtom;
@@ -31,7 +32,7 @@ import org.openscience.cdk.interfaces.IAtom;
 public class TestSignSIc {
 
     @Test
-    public void runSmartsBursiInclexclTest() throws BioclipseException, DSException{
+    public void runSignSicTest() throws BioclipseException, DSException{
 
         IDSManager ds = Activator.getDefault().getJavaManager();
         ICDKManager cdk=net.bioclipse.cdk.business.Activator.getDefault().getJavaCDKManager();
@@ -62,6 +63,54 @@ public class TestSignSIc {
 //        assertTrue( submatch.getMatchingAtoms().contains( 5 ) );
 //        assertTrue( submatch.getMatchingAtoms().contains( 8 ) );
 //        assertFalse( submatch.getMatchingAtoms().contains( 2 ) );
+
+    }
+
+    
+    @Test
+    public void runSignSicTestInJob() throws BioclipseException, DSException, InterruptedException{
+
+        IDSManager ds = Activator.getDefault().getJavaManager();
+        ICDKManager cdk=net.bioclipse.cdk.business.Activator.getDefault().getJavaCDKManager();
+
+        final ICDKMolecule mol = cdk.fromSMILES( "C1CCCCC1CC(CCC)CCC" );
+        ds.runTest( "signsic.bursi", mol, new BioclipseUIJob<List<ITestResult>>(){
+
+            @Override
+            public void runInUI() {
+                List<ITestResult> ret = getReturnValue();
+                assertNotNull( ret);
+
+                assertNotNull( ret);
+
+                System.out.println("=============================");
+                System.out.println("Results runSmartsBursiInclexclTestInJob:");
+                for (ITestResult res : ret){
+                    System.out.println(res);
+                }
+                System.out.println("=============================");
+
+                //Some QA
+                assertTrue( ret.size()==1 );
+                ITestResult testres = ret.get( 0 );
+                
+                assertTrue( testres instanceof ISubStructure );
+                ISubStructure submatch=(ISubStructure)testres;
+                
+                assertEquals( "HIT 1", testres.getName());
+                for (IAtom atom : submatch.getAtomContainer().atoms()){
+                    System.out.println("Atom: " + mol.getAtomContainer().getAtomNumber( atom ));
+                }
+//                assertTrue( submatch.getAtomContainer().ccontains( 1 ) );
+//                assertTrue( submatch.getMatchingAtoms().contains( 5 ) );
+//                assertTrue( submatch.getMatchingAtoms().contains( 8 ) );
+//                assertFalse( submatch.getMatchingAtoms().contains( 2 ) );
+
+                
+            }
+
+        });
+        
 
     }
 
