8fd299a72ec68d48a1f5b835866e66fcf1cea7a1
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-10 21:11:42 +0000

    Implemented PropertyCalculators for the active tests
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11253 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.ds.smarts.bursi/src/net/bioclipse/ds/smarts/bursi/SmartsMatchCalculator.java
@@ -0,0 +1,18 @@
+package net.bioclipse.ds.smarts.bursi;
+
+import net.bioclipse.ds.business.BaseDSPropertyCalculator;
+
+
+public class SmartsMatchCalculator extends BaseDSPropertyCalculator{
+
+    @Override
+    public String getPropertyName() {
+        return "net.bioclipse.ds.smarts.bursi";
+    }
+
+    @Override
+    public String getTestID() {
+        return "smarts.bursi";
+    }
+    
+}
