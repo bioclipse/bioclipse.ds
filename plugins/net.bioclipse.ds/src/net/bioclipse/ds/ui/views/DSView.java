ed499f856e95a1ef7c3e98cc50990339420a1dfc
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-10 19:45:40 +0000

    Removed unused imports.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11247 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ds/src/net/bioclipse/ds/ui/views/DSView.java
+++ plugins/net.bioclipse.ds/src/net/bioclipse/ds/ui/views/DSView.java
@@ -40,7 +40,6 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.help.IWorkbenchHelpSystem;
 import org.eclipse.ui.part.*;
