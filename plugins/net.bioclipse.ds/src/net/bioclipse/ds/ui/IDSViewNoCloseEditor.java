7370049d2ce7fe00939e9ab4cf052ac334da332f
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-11 22:21:03 +0000

    An interface for editors that should not blank DSView. Currently only implemented by WrappedBrowserEditor for Birt.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11266 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.ds/src/net/bioclipse/ds/ui/IDSViewNoCloseEditor.java
@@ -0,0 +1,10 @@
+package net.bioclipse.ds.ui;
+
+/**
+ * An interface that editors that should NOT close DSView should implement
+ * @author ola
+ *
+ */
+public interface IDSViewNoCloseEditor{
+
+}
