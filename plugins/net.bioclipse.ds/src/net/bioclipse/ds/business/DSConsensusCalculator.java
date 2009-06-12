9c2a757176564d3e1eb071fd1579f59429db6934
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-12 13:21:38 +0000

    Added a stub for a consensus calculator for DS.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11302 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.ds/src/net/bioclipse/ds/business/DSConsensusCalculator.java
@@ -0,0 +1,33 @@
+package net.bioclipse.ds.business;
+
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.cdk.ui.sdfeditor.business.IPropertyCalculator;
+
+
+public class DSConsensusCalculator implements IPropertyCalculator<String>{
+
+    public String calculate( ICDKMolecule molecule ) {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String getPropertyName() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String parse( String value ) {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public String toString( Object value ) {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
