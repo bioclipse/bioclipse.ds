fd38c479723e74577f349e7a7a11fd03e0de6765
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-06-15 15:25:05 +0000

    cdk.create is renamed to cdk.asCDKMolecule()
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@11358 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.ds.lipinski/src/net/bioclipse/ds/lipinski/impl/RuleOfFiveTest.java
+++ plugins/net.bioclipse.ds.lipinski/src/net/bioclipse/ds/lipinski/impl/RuleOfFiveTest.java
@@ -54,7 +54,7 @@ public class RuleOfFiveTest extends AbstractDSTest{
         ICDKMolecule cdkmol = null;
         ICDKMolecule cdkmol_in = null;
         try {
-            cdkmol_in = cdk.create( molecule );
+            cdkmol_in = cdk.asCDKMolecule( molecule );
             cdkmol=new CDKMolecule((IAtomContainer)cdkmol_in.getAtomContainer().clone());
 //            cdkmol = cdk.create( molecule );
         } catch ( BioclipseException e ) {
