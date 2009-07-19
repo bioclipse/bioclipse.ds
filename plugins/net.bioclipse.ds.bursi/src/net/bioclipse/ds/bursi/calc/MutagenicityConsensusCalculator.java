458ddb0964962dc1ab25daea01c7c05beafd82b7
Ola Spjuth <ola.spjuth@gmail.com>
2009-07-20 00:24:51 +0200

    Exyended new DSConsensusCalculator with a ConsensusTest for Mutagenicity endpoint.

--- plugins/net.bioclipse.ds.bursi/src/net/bioclipse/ds/bursi/calc/MutagenicityConsensusCalculator.java
+++ plugins/net.bioclipse.ds.bursi/src/net/bioclipse/ds/bursi/calc/MutagenicityConsensusCalculator.java
@@ -2,13 +2,28 @@ package net.bioclipse.ds.bursi.calc;
 
 import net.bioclipse.ds.impl.calc.DSConsensusCalculator;
 
-
+/**
+ * 
+ * @author ola
+ *
+ */
 public class MutagenicityConsensusCalculator extends DSConsensusCalculator {
+    
+    
 
     @Override
+    public String getPropertyName() {
+        return "Mutagenicity Consensus";
+    }
+    
+    @Override
     protected String getTestID() {
-
         return "bursi.consensus";
     }
+    
+    protected String getEndpoint(){
+        return "net.bioclipse.ds.mutagenicity";
+    }
+
 
 }
