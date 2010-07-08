f8dde73a98029267e8f032b097d250299f9dcf58
Ola Spjuth <ola.spjuth@gmail.com>
2010-07-08 09:02:44 +0200

    Started working on Signatures tests.

--- plugins/net.bioclipse.ds.signatures.tests/src/net/bioclipse/ds/signatures/tests/AbstractSignaturesManagerPluginTest.java
+++ plugins/net.bioclipse.ds.signatures.tests/src/net/bioclipse/ds/signatures/tests/AbstractSignaturesManagerPluginTest.java
@@ -16,10 +16,18 @@ import net.bioclipse.cdk.business.Activator;
 import net.bioclipse.cdk.business.ICDKManager;
 import net.bioclipse.cdk.domain.ICDKMolecule;
 import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.ds.signatures.CDKMoleculeSignatureAdapter;
 import net.bioclipse.ds.signatures.business.ISignaturesManager;
 import net.bioclipse.ds.signatures.prop.calc.AtomSignatures;
 
 import org.junit.Test;
+import org.openscience.cdk.Atom;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IBond;
+
+import signature.chemistry.MoleculeSignature;
 
 /**
  * 
@@ -44,5 +52,33 @@ public abstract class AbstractSignaturesManagerPluginTest {
         assertEquals( "WEEE", sp );
         
     }
+    
+    
+    @Test
+    public void benzene() {
+       IAtomContainer benzene = new AtomContainer();
+       benzene.addAtom(new Atom("C"));
+       benzene.addAtom(new Atom("C"));
+       benzene.addAtom(new Atom("C"));
+       benzene.addAtom(new Atom("C"));
+       benzene.addAtom(new Atom("C"));
+       benzene.addAtom(new Atom("C"));
+       benzene.addBond(0, 1, IBond.Order.SINGLE);
+       benzene.addBond(1, 2, IBond.Order.SINGLE);
+       benzene.addBond(2, 3, IBond.Order.SINGLE);
+       benzene.addBond(3, 4, IBond.Order.SINGLE);
+       benzene.addBond(4, 5, IBond.Order.SINGLE);
+       benzene.addBond(5, 0, IBond.Order.SINGLE);
+       for (IBond bond : benzene.bonds()) {
+           bond.setFlag(CDKConstants.ISAROMATIC, true);
+       }
+       
+       signature.chemistry.Molecule mol =
+           CDKMoleculeSignatureAdapter.convert(benzene);
+       MoleculeSignature molSig = new MoleculeSignature(mol);
+       System.out.println(molSig.toCanonicalString());
+    }
+
+
 
 }
