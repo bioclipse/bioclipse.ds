b0538cf7c150970e79f4be5d18a1128cfbdde6a8
Ola Spjuth <ola.spjuth@gmail.com>
2010-07-08 10:22:26 +0200

    Wrote tests for AtomSignatures and MolecularSignature. * Use an SDF with precomputed Signatures as expected values

--- plugins/net.bioclipse.ds.signatures.tests/src/net/bioclipse/ds/signatures/tests/AbstractSignaturesManagerPluginTest.java
+++ plugins/net.bioclipse.ds.signatures.tests/src/net/bioclipse/ds/signatures/tests/AbstractSignaturesManagerPluginTest.java
@@ -12,6 +12,13 @@ package net.bioclipse.ds.signatures.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
 import net.bioclipse.cdk.business.Activator;
 import net.bioclipse.cdk.business.ICDKManager;
 import net.bioclipse.cdk.domain.ICDKMolecule;
@@ -20,6 +27,8 @@ import net.bioclipse.ds.signatures.CDKMoleculeSignatureAdapter;
 import net.bioclipse.ds.signatures.business.ISignaturesManager;
 import net.bioclipse.ds.signatures.prop.calc.AtomSignatures;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
 import org.junit.Test;
 import org.openscience.cdk.Atom;
 import org.openscience.cdk.AtomContainer;
@@ -36,48 +45,100 @@ import signature.chemistry.MoleculeSignature;
  */
 public abstract class AbstractSignaturesManagerPluginTest {
 
-    protected static ISignaturesManager managerNamespace;
-    
-    /**
-     * Test generate signatures for a single molecule
-     * 
-     * @throws BioclipseException
-     */
-    @Test public void testDoSomething() throws BioclipseException {
+	protected static ISignaturesManager managerNamespace;
 
-        ICDKManager cdk = Activator.getDefault().getJavaCDKManager();
-        ICDKMolecule mol=cdk.fromSMILES( "C1CCCCC1CC(CC)" );
-        AtomSignatures sp = managerNamespace.generate( mol );
-        assertTrue( sp.getSignatures().size()==1 );
-        assertEquals( "WEEE", sp );
-        
-    }
-    
-    
-    @Test
-    public void benzene() {
-       IAtomContainer benzene = new AtomContainer();
-       benzene.addAtom(new Atom("C"));
-       benzene.addAtom(new Atom("C"));
-       benzene.addAtom(new Atom("C"));
-       benzene.addAtom(new Atom("C"));
-       benzene.addAtom(new Atom("C"));
-       benzene.addAtom(new Atom("C"));
-       benzene.addBond(0, 1, IBond.Order.SINGLE);
-       benzene.addBond(1, 2, IBond.Order.SINGLE);
-       benzene.addBond(2, 3, IBond.Order.SINGLE);
-       benzene.addBond(3, 4, IBond.Order.SINGLE);
-       benzene.addBond(4, 5, IBond.Order.SINGLE);
-       benzene.addBond(5, 0, IBond.Order.SINGLE);
-       for (IBond bond : benzene.bonds()) {
-           bond.setFlag(CDKConstants.ISAROMATIC, true);
-       }
-       
-       signature.chemistry.Molecule mol =
-           CDKMoleculeSignatureAdapter.convert(benzene);
-       MoleculeSignature molSig = new MoleculeSignature(mol);
-       System.out.println(molSig.toCanonicalString());
-    }
+	/**
+	 * Test generate atom signatures and molecular signatures
+	 * for a bunch of molecules with expected values stored as
+	 * properties in an SDFile
+	 * 
+	 * @throws BioclipseException
+	 * @throws URISyntaxException 
+	 * @throws IOException 
+	 * @throws MalformedURLException 
+	 * @throws CoreException 
+	 */
+	@Test
+	public void testAtomSignature() throws BioclipseException, URISyntaxException, MalformedURLException, IOException, CoreException {
+
+		ICDKManager cdk = Activator.getDefault().getJavaCDKManager();
+
+		URI uri = getClass().getResource("/testdata/3drugs_sign.sdf").toURI();
+        URL url = FileLocator.toFileURL(uri.toURL());
+        String path = url.getFile();
+        List<ICDKMolecule> mols = cdk.loadMolecules( path);
+        for (ICDKMolecule mol : mols){
+        	System.out.println("Testing mol: " + mol.getName());
+        	testSignaturesAgainstProperties(mol);
+        }
+	}
+
+
+	private void testSignaturesAgainstProperties(ICDKMolecule mol) throws BioclipseException {
+		//Get the properties
+    	String ap0=(String) mol.getAtomContainer().getProperty("Atom Signatures height 0");
+    	assertNotNull(ap0);
+    	String ap1=(String) mol.getAtomContainer().getProperty("Atom Signatures height 1");
+    	assertNotNull(ap1);
+    	String ap2=(String) mol.getAtomContainer().getProperty("Atom Signatures height 2");
+    	assertNotNull(ap2);
+    	String ap3=(String) mol.getAtomContainer().getProperty("Atom Signatures height 3");
+    	assertNotNull(ap3);
+    	String ap4=(String) mol.getAtomContainer().getProperty("Atom Signatures height 4");
+    	assertNotNull(ap4);
+    	String ap5=(String) mol.getAtomContainer().getProperty("Atom Signatures height 5");
+    	assertNotNull(ap5);
+    	String mp=(String) mol.getAtomContainer().getProperty("Molecular Signature");
+    	assertNotNull(mp);
+
+    	//Calculate Signatures
+		AtomSignatures as = managerNamespace.generate( mol );
+		assertEquals(ap1, as.toString());
+		AtomSignatures as0 = managerNamespace.generate( mol,0 );
+		assertEquals(ap0, as0.toString());
+		AtomSignatures as1 = managerNamespace.generate( mol,1 );
+		assertEquals(ap1, as1.toString());
+		AtomSignatures as2 = managerNamespace.generate( mol,2 );
+		assertEquals(ap2, as2.toString());
+		AtomSignatures as3 = managerNamespace.generate( mol,3 );
+		assertEquals(ap3, as3.toString());
+		AtomSignatures as4 = managerNamespace.generate( mol,4 );
+		assertEquals(ap4, as4.toString());
+		AtomSignatures as5 = managerNamespace.generate( mol,5 );
+		assertEquals(ap5, as5.toString());
+		String ms = managerNamespace.generateMoleculeSignature(mol);
+		assertEquals(mp, ms);
+    	
+	}
+
+
+	@Test
+	public void testCDKSignaturesAdapter() {
+
+		IAtomContainer benzene = new AtomContainer();
+		benzene.addAtom(new Atom("C"));
+		benzene.addAtom(new Atom("C"));
+		benzene.addAtom(new Atom("C"));
+		benzene.addAtom(new Atom("C"));
+		benzene.addAtom(new Atom("C"));
+		benzene.addAtom(new Atom("C"));
+		benzene.addBond(0, 1, IBond.Order.SINGLE);
+		benzene.addBond(1, 2, IBond.Order.SINGLE);
+		benzene.addBond(2, 3, IBond.Order.SINGLE);
+		benzene.addBond(3, 4, IBond.Order.SINGLE);
+		benzene.addBond(4, 5, IBond.Order.SINGLE);
+		benzene.addBond(5, 0, IBond.Order.SINGLE);
+		for (IBond bond : benzene.bonds()) {
+			bond.setFlag(CDKConstants.ISAROMATIC, true);
+		}
+
+		signature.chemistry.Molecule mol =
+			CDKMoleculeSignatureAdapter.convert(benzene);
+		MoleculeSignature molSig = new MoleculeSignature(mol);
+		assertTrue(molSig.toCanonicalString().length()>0);
+		System.out.println("Translated Benzene has Signature: " + 
+				molSig.toCanonicalString());
+	}
 
 
 
