38b2004b7f69aea7d54f613998c7ba877c1bd705
Ola Spjuth <ola.spjuth@gmail.com>
2010-07-23 15:55:35 +0200

    Implemented rematching of original ames data file

--- plugins/net.bioclipse.ds.ames.test/src/net/bioclipse/ds/ames/test/TestAmesPredictions.java
+++ plugins/net.bioclipse.ds.ames.test/src/net/bioclipse/ds/ames/test/TestAmesPredictions.java
@@ -1,13 +1,18 @@
 package net.bioclipse.ds.ames.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import net.bioclipse.cdk.business.ICDKManager;
 import net.bioclipse.cdk.domain.ICDKMolecule;
@@ -17,6 +22,8 @@ import net.bioclipse.ds.Activator;
 import net.bioclipse.ds.business.IDSManager;
 import net.bioclipse.ds.model.DSException;
 import net.bioclipse.ds.model.ITestResult;
+import net.bioclipse.ds.model.result.ExternalMoleculeMatch;
+import net.bioclipse.ds.signatures.business.ISignaturesManager;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
@@ -85,8 +92,8 @@ public class TestAmesPredictions {
 
 	}
 	
-	
-	
+
+
 	@Test
 	public void testAmesNN() throws BioclipseException, DSException, URISyntaxException, MalformedURLException, IOException, CoreException{
 
@@ -145,21 +152,83 @@ public class TestAmesPredictions {
 
 	}
 	
+	
 
 	/**
-	 * Read 
-	 * @param path to molecule on Classpath
-	 * @return ICDKMolecule
+	 * Rematch all in original data to verify at least one exact match
+	 * @throws InvocationTargetException 
 	 */
-	private ICDKMolecule loadMolecule(String path)
-	throws IOException, MalformedURLException, BioclipseException,
-	CoreException, URISyntaxException {
+	@Test
+	public void testAmesExacReMatchSDF() throws BioclipseException, DSException, URISyntaxException, MalformedURLException, IOException, CoreException, InvocationTargetException{
+	
+	IDSManager ds = Activator.getDefault().getJavaManager();
+	ICDKManager cdk = 
+		net.bioclipse.cdk.business.
+		Activator.getDefault().getJavaCDKManager();
+	ISignaturesManager signatures=net.bioclipse.ds.signatures.Activator.getDefault().getJavaSignaturesManager();
 
-		URI uri = getClass().getResource(path).toURI();
+	//Exact match should work for all original molecules
+	Iterator<ICDKMolecule> it = cdk.createMoleculeIterator(sourcePathtoAbsolutePath("/data/bursi_nosalts.sdf"));
+	
+	int cnt=0;
+	Map<Integer, List<ITestResult>> errors=new HashMap<Integer, List<ITestResult>>();
+	while (it.hasNext()){
+		ICDKMolecule mol =it.next();
+		
+		List<ITestResult> ret = ds.runTest( "ames.lookup.exact", mol );
+		
+		if (ret.size()!=1){
+			errors.put(cnt, ret);
+			String ms = signatures.generateMoleculeSignature(mol);
+			System.out.println("Mol anomaly, index " + cnt + " with signature: " + ms);
+		}
+//		assertTrue("SDF index " + cnt +" did not have an exact match",
+//				ret.size()>=1 );
+		cnt++;
+	}
+
+	//Just debug out anomalies, where we have multiple matches
+	System.out.println("We have " + errors.size() + " anomalies.");
+	for (Integer i : errors.keySet()){
+		System.out.println("Mol: " + i + " had anomalities...");
+		if (errors.get(i)==null || errors.get(i).size()==0)
+			System.out.println("    ...no exact matches were found");
+		else{
+			for (ITestResult tres : errors.get(i)){
+				ExternalMoleculeMatch ematch=(ExternalMoleculeMatch)tres;
+				String matchmolsign="";
+				try{
+					matchmolsign=signatures.generateMoleculeSignature(ematch.getMatchedMolecule());
+				}catch (Exception e){
+					matchmolsign="Error: " + e.getMessage();
+				}
+				System.out.println("   Match: " + ematch.getName() + " with sign: " + matchmolsign);
+			}
+		}
+	}
+
+
+//	//Just test a mol with no matches; adrenaline
+//	List<ITestResult> ret = ds.runTest( "ames.lookup.exact", 
+//			cdk.fromSMILES("Oc1ccc(cc1O)[C@@H](O)CNC") );
+//	assertEquals( 0, ret.size() );
+		
+}
+
+	
+	private ICDKMolecule loadMolecule(String path) 
+	throws MalformedURLException, IOException, BioclipseException, 
+	CoreException, URISyntaxException {
 		ICDKManager cdk=net.bioclipse.cdk.business.Activator.getDefault().getJavaCDKManager();
+		return cdk.loadMolecule(sourcePathtoAbsolutePath(path));
+	}
+
+	
+	private String sourcePathtoAbsolutePath(String sourcepath) 
+	throws URISyntaxException, MalformedURLException, IOException{
+		URI uri = getClass().getResource(sourcepath).toURI();
 		URL url = FileLocator.toFileURL(uri.toURL());
-		String p_path = url.getFile();
-		return cdk.loadMolecule(p_path);
+		return url.getFile();
 	}
 
 
