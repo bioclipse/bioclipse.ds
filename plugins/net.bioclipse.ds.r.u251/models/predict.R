d84fd3ca3c1fc5e0fd5485a6dc4afd0dff29c6e9
Ola Spjuth <ola.spjuth@gmail.com>
2011-12-02 18:23:56 +0100

    removed old models

--- plugins/net.bioclipse.ds.r.u251/models/predict.R
+++
@@ -1,4 +0,0 @@
-u251.tmp <- predict(u251.naAndStdvTreatment, %input%)
-u251.tmp <- predict(u251.imputed, u251.tmp)
-names(u251.tmp) <- rownames(u251.rf$importance)
-predict(u251.rf, t(u251.tmp), type="prob")[2]
