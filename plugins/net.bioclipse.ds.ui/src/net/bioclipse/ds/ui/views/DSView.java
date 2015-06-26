a9888e7eb481a2bbfaf79f7de6040f3e4a8a2324
Arvid Berg <goglepox@gmail.com>
2015-06-26 16:51:49 +0200

    Remove toplevel from dsview if default is the only one

--- plugins/net.bioclipse.ds.ui/src/net/bioclipse/ds/ui/views/DSView.java
+++ plugins/net.bioclipse.ds.ui/src/net/bioclipse/ds/ui/views/DSView.java
@@ -469,9 +469,11 @@ public class DSView extends ViewPart implements IPartListener2, IPropertyChangeL
                             //Init viewer with available endpoints
                             IDSManager ds = net.bioclipse.ds.Activator.getDefault().getJavaManager();
                             try {
-                            	if (ds.getFullTopLevels().size()>0) //There is always the uncategorized...
+                            	if (ds.getFullTopLevels().size()>1) //There is always the uncategorized...
                             		viewer.setInput(ds.getFullTopLevels().toArray());
-                            	else{
+                            	else if(ds.getFullTopLevels().size()==1 && ds.getFullEndpoints().size()>1) {
+                            	    viewer.setInput( ds.getFullEndpoints() );
+                            	} else {
                             		String[] msg = new String[2];
                             		msg[0]="   No models available.";
                             		msg[1]="link:Install models...";// from menu: 'Install > DS Models...'";
